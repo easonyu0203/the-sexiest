95d5975378065dbe56d954c870f91e12690d0cb4
Sherry Yeh <b08705053@ntu.edu.tw>
2021-01-16 19:39:55 +0800

    added home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,35 @@
 import React from "react";
+import '../App.css';
+import { Button } from '../components/Button';
+import './Home.css';
 
 export default function Home() {
   return (
     <>
-      <div>this is home page</div>
+      <div className='home-container'>
+        <h1>THE SEXIEST</h1>
+        <p>小標 我不知道寫啥</p>
+        <div className='home-btns'>
+          <Button
+            className='btns'
+            buttonStyle='btn--outline'
+            buttonSize='btn--large'
+            value='play'
+          >
+            GET STARTED
+          </Button>
+          <Button
+            className='btns'
+            buttonStyle='btn--outline'
+            buttonSize='btn--large'
+            value='tutorial'
+          >
+            TUTORIAL 
+          </Button>
+        </div>
+      </div>
     </>
   );
 }
+
+
